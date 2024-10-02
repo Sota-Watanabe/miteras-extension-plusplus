01d5c94f7f4dbe96840c67546c26878dffbe796e
Sota-Watanabe <stronger.riceball@gmail.com>
2024-10-02 18:32:46 +0900

    ✨feature: 2024/10 に対応

--- src/model/target-projects.ts
+++ src/model/target-projects.ts
@@ -1,10 +1,10 @@
 export const targetProjects = {
   hiproToB: {
-    value: 'TSGCC06A40',
-    label: 'TSGCC06A40 [TS共通]HiPro法人_開発(資産化)_24年度2Q',
+    value: 'ITICC06A40',
+    label: 'ITICC06A40 [TS共通]HiPro法人_開発(資産化)_24年度3Q',
   },
   hiproToC: {
-    value: 'TSGCC07A40',
-    label: 'TSGCC07A40 [TS共通]HiPro個人_開発(資産化)_24年度2Q',
+    value: 'ITICC07A40',
+    label: 'ITICC07A40 [TS共通]HiPro個人_開発(資産化)_24年度3Q',
   },
 };
