e4537fce2bd0816def101c3bcab17012ee6a44a5
Sota-Watanabe <stronger.riceball@gmail.com>
2025-08-07 00:08:36 +0900

    ✨feature: 新しいボタンと画像を追加し、スタイルを適用

--- src/hooks/use-utils.ts
+++ src/hooks/use-utils.ts
@@ -3,14 +3,27 @@ export const sleep = (msec: number) =>
 
 export const imagePath = (name: string) => chrome.runtime.getURL(name);
 
-export const autoInputButton = `<button class="btn btnAction btnAction--L">
+export const autoInputButton = `<button class="btn btnAction btnAction--L" id="auto-input-button">
 <img height="19" src="${imagePath(
   'hipro.png'
 )}" width="19" alt="Calt It!"><span> Calc It! </span>
 </button>&nbsp;&nbsp;`;
 
-export const resetButton = `<button class="btn btnAction btnAction--L">
+export const resetButton = `<button class="btn btnAction btnAction--L" id="reset-button">
 <img height="19" src="${imagePath(
   'reset.png'
 )}" width="19" alt="set 0!"><span> Set 0! </span>
 </button>&nbsp;&nbsp;`;
+
+export const setPcTimeButton = `<button type="button" class="btn btnAction btnAction--S u_maL7 char2 delete-breaktime-btn" id="set-pc-time-button">
+<span style="line-height: normal;">PC時間 <span>
+<img height="15" src="${imagePath('pc.png')}" width="19" alt="set 0!">
+</button>`;
+
+export const autoRunButton = `
+<button type="button" class="btn btnAction btnAction--L request-approval-button u_maL7" id="auto-run-all" isholiday="false" isrequestawdisabled="true" isnull="true" isnull2="true" style="padding: 0;">
+<img src="${imagePath('robot_face.png')}" width="30" height="30" style="
+    margin: 0;
+"> 
+</button>
+`;
--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,7 +1,13 @@
 import { LogError, LogInfo, LogWarn } from '../../hooks/use-logger';
 import { setProjectValue } from '../../hooks/use-miteras-set';
 import { resetWorkTime } from '../../hooks/use-reset-work-time';
-import { autoInputButton, resetButton, sleep } from '../../hooks/use-utils';
+import {
+  autoInputButton,
+  autoRunButton,
+  resetButton,
+  setPcTimeButton,
+  sleep,
+} from '../../hooks/use-utils';
 import { targetProjects } from '../../model/target-projects';
 
 LogInfo('content');
@@ -15,18 +21,20 @@ document.addEventListener('DOMContentLoaded', (_) => {
     )
     .forEach((x, i, o) => {
       LogInfo(`Add event handler ${i + 1}/${o.length}`);
-      x.addEventListener('click', handler);
+      LogInfo('x', x);
+      x.addEventListener('click', () => handler(x));
     });
 });
 
 let time = 0;
-const handler = async () => {
-  LogInfo('setAutoInputButton');
+const handler = async (buttonElement: Element) => {
   // モーダル出現待ち
   await sleep(500);
-
   setAutoInputButton();
   setWorkTimeResetButton();
+  setPcTimeSetButton();
+  setAutoRunButton();
+  deleteSaveButtonAlert();
 
   // モーダル上で更新が走った場合もダミーボタンをセットする
 
@@ -53,11 +61,197 @@ const handler = async () => {
     setWorkTimeResetButton();
     LogInfo('前日をコピー時に勤務終了時間を自動でセット');
     autoWorkTimeOutSet();
+    setPcTimeSetButton();
+    setAutoRunButton();
+    deleteSaveButtonAlert();
   });
   observer.observe(modalElem, {
     childList: true,
     subtree: true,
   });
+};
+
+const deleteSaveButtonAlert = () => {
+  const saveButton = document.getElementById('request-approval-all');
+
+  if (saveButton != null) {
+    // mouseoverイベントを削除
+    LogInfo('保存ボタンのmouseoverイベントを削除', saveButton);
+    saveButton.addEventListener(
+      'mouseover',
+      (event) => {
+        event.stopImmediatePropagation();
+      },
+      true
+    );
+  }
+};
+
+const setAutoRunButton = () => {
+  // クラス modalAction__btnBox を持つ要素を取得
+  const modalActionBtnBox = document.querySelector('.modalAction__btnBox');
+  // 最後の子要素に自動実行ボタンを追加
+  if (!modalActionBtnBox) {
+    LogWarn('自動実行ボタンを配置する要素がありません');
+    return;
+  }
+  LogInfo('自動実行ボタンを用意');
+  const dummyButtonNode = document.createElement('span');
+  dummyButtonNode.innerHTML = autoRunButton;
+  dummyButtonNode.onclick = async () => {
+    LogInfo('自動実行ボタンがクリックされました');
+    // copy-previous-day
+    const copyButtonNode = document.querySelector(
+      '[id="copy-previous-day"]'
+    ) as HTMLButtonElement;
+    if (!copyButtonNode) {
+      LogError('前日をコピーボタンが見つかりません');
+      return;
+    }
+    copyButtonNode.click();
+
+    const isCopyCompleted = await checkCopyCompletion();
+    if (!isCopyCompleted) {
+      LogError('前日をコピーが完了しませんでした');
+      return;
+    }
+
+    // set-pc-time-button
+    await sleep(100);
+    const setPcTimeButton = document.querySelector(
+      '#set-pc-time-button'
+    ) as HTMLButtonElement;
+    if (setPcTimeButton) {
+      setPcTimeButton.click();
+    } else {
+      LogError('PC時間設定ボタンが見つかりません');
+    }
+
+    // set-pc-time-button
+    await sleep(100);
+    const resetButton = document.querySelector(
+      '#reset-button'
+    ) as HTMLButtonElement;
+    if (resetButton) {
+      resetButton.click();
+    } else {
+      LogError('リセットボタンが見つかりません');
+    }
+
+    // auto-input-button
+    await sleep(100);
+    const autoInputButton = document.querySelector(
+      '#auto-input-button'
+    ) as HTMLButtonElement;
+    if (autoInputButton) {
+      autoInputButton.click();
+    } else {
+      LogError('自動入力ボタンが見つかりません');
+    }
+  };
+  // 最後に追加
+  const children = modalActionBtnBox.children;
+  if (children.length > 1) {
+    modalActionBtnBox.insertBefore(dummyButtonNode, children[children.length]);
+  } else {
+    LogWarn('ボタンが1つしかありません。ダミーボタンを追加できません');
+  }
+};
+
+// 0.5秒ごとにボタンの状態をチェックし、disabledになったら次の処理へ
+// 10秒経ってもdisabledにならなければエラー
+const checkCopyCompletion = async () => {
+  const maxAttempts = 20; // 10秒 ÷ 0.5秒 = 20回
+  let attempts = 0;
+
+  while (attempts < maxAttempts) {
+    await sleep(500); // 0.5秒待機
+    attempts++;
+
+    const afterCopyButtonNode = document.querySelector(
+      '[id="copy-previous-day"]'
+    ) as HTMLButtonElement;
+
+    if (
+      afterCopyButtonNode &&
+      afterCopyButtonNode.getAttribute('disabled') === 'disabled'
+    ) {
+      LogInfo('前日をコピーが完了しました');
+      return true;
+    }
+  }
+
+  return false; // 10秒経ってもdisabledにならなかった場合
+};
+
+const setPcTimeSetButton = () => {
+  // fieldsetタグのscheduler-borderクラスを取得
+  const fieldsetElem = document.querySelector('fieldset.scheduler-border');
+  if (!fieldsetElem) {
+    LogWarn('pc時間自動入力ボタンを配置する要素がありません');
+    return;
+  }
+  LogInfo('pc時間自動入力ボタンを用意');
+  const dummyButtonNode = document.createElement('span');
+  dummyButtonNode.innerHTML = setPcTimeButton;
+  dummyButtonNode.onclick = onClickPcTimeSetButton;
+  // fieldsetElemの1番目のdiv要素の3番目にdummyButtonNodeを追加
+  const firstDiv = fieldsetElem.querySelector('div');
+  if (firstDiv && firstDiv.children.length >= 2) {
+    firstDiv.insertBefore(dummyButtonNode, firstDiv.children[2]);
+  } else if (firstDiv) {
+    firstDiv.appendChild(dummyButtonNode);
+  }
+};
+
+const onClickPcTimeSetButton = () => {
+  // 日付文字列
+  const dateElem = document.querySelector('#daily-entry-date');
+  if (!dateElem) {
+    LogWarn('日付要素がありません');
+    return;
+  }
+  const date = dateElem.textContent?.trim().slice(-2) || '';
+  // dateの後ろに文字を取得
+
+  // table01__cell--dateクラスの要素で、テキストの先頭2文字がdateと一致する要素を取得
+  const dateNode = Array.from(
+    document.querySelectorAll('.table01__cell--date')
+  ).find((elem) => {
+    const text = elem.textContent?.trim() || '';
+    return text.startsWith(date);
+  });
+  if (!dateNode) {
+    LogWarn('日付ノードが見つかりません');
+    return;
+  }
+
+  // bottonElementの親の要素と同じ階層の table01__cell--time クラスの3つ目と4つ目のテキストを取得
+  const parentRow = dateNode.closest('tr');
+
+  const timeCells = parentRow?.querySelectorAll('.table01__cell--time');
+  const pcStartTimeText = timeCells?.[2]?.textContent?.trim() || '';
+  const pcEndTimeText = timeCells?.[3]?.textContent?.trim() || '';
+  LogInfo('3つ目の時間テキスト:', pcStartTimeText);
+  LogInfo('4つ目の時間テキスト:', pcEndTimeText);
+
+  // idがwork-time-inのinput要素にpcStartTimeTextをセット
+  const workTimeInNode = document.querySelector<HTMLInputElement>(
+    '[id="work-time-in"]'
+  );
+  if (workTimeInNode) {
+    workTimeInNode.value = pcStartTimeText;
+  }
+  // idがwork-time-outのinput要素にpcEndTimeTextをセット
+  const workTimeOutNode = document.querySelector<HTMLInputElement>(
+    '[id="work-time-out"]'
+  );
+  if (workTimeOutNode) {
+    workTimeOutNode.value = pcEndTimeText;
+    // 一度選択状態にする。しないと実働時間が更新されず500エラーが発生する
+    workTimeOutNode.focus();
+    workTimeOutNode.blur();
+  }
 };
 
 const autoWorkTimeOutSet = () => {
