e4e2c159b72f4eb31bcc4176033ea0334729f306
Sota-Watanabe <stronger.riceball@gmail.com>
2026-08-31 19:32:20 +0900

    ✨feature: ターゲットプロジェクトの値と比率に説明コメントを追加

--- src/model/target-projects.ts
+++ src/model/target-projects.ts
@@ -1,18 +1,18 @@
 export const targetProjects = [
   {
-    value: 'ITICR01A30',
+    value: 'ITICR01A30', // ①継続開発：10%
     ratio: 10,
   },
   {
-    value: 'ITICT01A10',
-    ratio: 10,
+    value: 'ITICT01A10', // ②要件定義・人選軸AI：5％
+    ratio: 5,
   },
   {
-    value: 'ITICT02A10',
-    ratio: 10,
+    value: 'ITICC01A20', // ③サイト統合：35%
+    ratio: 35,
   },
   {
-    value: 'ITICC01A20',
-    ratio: 70,
+    value: 'ITICT02A10', // ④窓口開設自動化：40%
+    ratio: 40,
   },
 ];
