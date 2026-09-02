5d424d02b35154ec85e6bd98b4e33a12fe5b003f
Sota-Watanabe <stronger.riceball@gmail.com>
2026-09-02 13:14:00 +0900

    ✨feature: バージョンを3.3.5から3.3.6に更新し、ターゲットプロジェクトの値と比率を修正

--- src/model/target-projects.ts
+++ src/model/target-projects.ts
@@ -1,18 +1,21 @@
+// https://persol-service-dev.slack.com/archives/C04U6FE9QQK/p1788225059283409?thread_ts=1788165588.843239&cid=C04U6FE9QQK
+// 継続開発、個人サイト統合、窓口自動化、要件定義、人選軸AIを
+// 2 : 7 : 8 : 1で配分
 export const targetProjects = [
   {
-    value: 'ITICR01A30', // ①継続開発：10%
-    ratio: 10,
+    value: 'ITICR01A30', // ①継続開発(HiPro共通マイページ継続開発)：11.11%
+    ratio: 11.11,
   },
   {
-    value: 'ITICT01A10', // ②要件定義・人選軸AI：5％
-    ratio: 5,
+    value: 'ITICC01A20', // ②個人サイト統合(HiPro個人サイト統合PJTフェーズ１_開発)：38.89%
+    ratio: 38.89,
   },
   {
-    value: 'ITICC01A20', // ③サイト統合：35%
-    ratio: 35,
+    value: 'ITICT02A10', // ③窓口開設自動化(窓口開設自動化PJT_企画)：44.44%
+    ratio: 44.44,
   },
   {
-    value: 'ITICT02A10', // ④窓口開設自動化：40%
-    ratio: 40,
+    value: 'ITICT01A10', // ④要件定義・人選軸AI：5.56%
+    ratio: 5.56,
   },
 ];
